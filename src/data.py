75f776467e21ebd8747012e1b60c6e16ba5138ba
Parth <pthosan@ncsu.edu>
2023-04-02 11:54:18 -0400

    added k means

--- src/data.py
+++ src/data.py
@@ -1,9 +1,15 @@
 import os
-
 import numpy as np
 import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.model_selection import train_test_split
+import warnings
+warnings.filterwarnings("ignore")
+import math
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+import matplotlib.pyplot as plt
+
 
 from src.pre_process import (
     CATEGORICAL_COLUMNS,
@@ -61,9 +67,38 @@ def cluster(x, y):
     x_train, x_test, y_train, y_test = train_test_split(
         x, y, test_size=0.33, random_state=42
     )
-    _model = KMeans()
-    _model.fit(x_train, y_train)
-    return _model
+    # _model = KMeans()
+    # _model.fit(x_train, y_train)
+    # # print the centroids of the clusters
+    # # print(_model.cluster_centers_)
+    #
+    # # predict the cluster labels for the data points
+    # print(_model.labels_)
+
+    # pipe = Pipeline([('imputer', SimpleImputer(strategy='mean')),
+    #                  ('kmeans', KMeans(n_clusters=5))])
+
+    # fit the pipeline to the data
+    # pipe.fit(x_train)
+
+    # predict the cluster labels for the data points
+    # print(pipe.named_steps['kmeans'].cluster_centers_)
+    # print(pipe.named_steps['kmeans'].labels_)
+
+    wcss = []
+    for k in range(1, 11):
+        pipe = Pipeline([('imputer', SimpleImputer(strategy='mean')),
+                         ('kmeans', KMeans(n_clusters=k, init='k-means++', max_iter=300, n_init=10, random_state=0))])
+
+        # fit the pipeline to the data
+        pipe.fit(x_train)
+        wcss.append(pipe.named_steps['kmeans'].inertia_)
+    plt.plot(range(1, 11), wcss)
+    plt.title('Elbow Method')
+    plt.xlabel('Number of clusters')
+    plt.ylabel('WCSS')
+    plt.show()
+
 
 
 # model = cluster(*split_x_y(read_csv()))
@@ -78,6 +113,12 @@ for i in data_dict:
     print(f"File Name: {i}")
     x, y = split_x_y(data_dict[i])
     print("Printing X:")
-    print(x.head())
+    # print(x.head())
     print("Printing Y:")
-    print(y.head())
+    # print(y.head())
+
+    # df = pd.read_csv(DATA_DIR+i)
+    #
+    # print(df.isnull().sum())
+
+    cluster(x,y)
