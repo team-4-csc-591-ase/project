e3481fd642df1d7ac31120ee192726d9e4820b5d
Ritwik Tiwari <ritwiktiwari24@gmail.com>
2023-04-09 16:45:17 -0400

    Changes by thosaniparth

--- src/clustering.py
+++ src/clustering.py
@@ -26,7 +26,69 @@ def half(data, rows=None, cols=None, above=None):
         return query.dist(data, r1, r2, cols)
 
     def cos(a, b, c):
-        return (a**2 + c**2 - b**2) / (2 * c)
+        if not c==0:
+            return (a**2 + c**2 - b**2) / (2 * c)
+        else:
+            return 1
+
+    def euclidean_distance(a, b, c):
+        """
+        Returns the Euclidean distance between points A and C in a triangle with sides a, b, and c, using the Pythagorean theorem.
+        """
+        if not b == 0:
+            x = (a ** 2 - c ** 2 + b ** 2) / (2 * b)
+            y = math.sqrt(abs(a ** 2 - x ** 2))
+            distance = math.sqrt(x ** 2 + y ** 2)
+            # Calculate the maximum possible distance between points A and C
+            max_distance = c / 2
+            # Scale the distance to the range [0, 1]
+            scaled_distance = distance / max_distance
+            return scaled_distance
+        else:
+            return 1
+
+    def manhattan_distance_scaled(a, b, c):
+        """
+        Returns the Manhattan distance between points A and C in a triangle with sides a, b, and c, scaled to the range [0, 1].
+        """
+        if not a==0:
+            # Calculate the x-coordinate of point B
+            x = (a ** 2 + b ** 2 - c ** 2) / (2 * a)
+
+            # Calculate the Manhattan distance between points A and C
+            distance = abs(a - x) + abs(b)
+
+            # Calculate the maximum possible Manhattan distance between points A and C
+            max_distance = a + b
+
+            # Scale the distance to the range [0, 1]
+            scaled_distance = distance / max_distance
+
+            return scaled_distance
+        else:
+            return 1
+
+    def hamming_distance(a, b, c):
+        """
+        Returns the Hamming distance between the points of a triangle with sides of lengths a, b, and c.
+        Assumes that the points are scaled to the range [0, 1].
+        """
+        if not c==0:
+            x1, y1 = 0, 0  # the first point is always (0, 0)
+            x2, y2 = c, 0  # the second point is (c, 0)
+            x3 = (a ** 2 - b ** 2 + c ** 2) / (2 * c)  # use cosine rule to find x-coordinate of third point
+            y3 = np.sqrt(a ** 2 - x3 ** 2)  # use Pythagorean theorem to find y-coordinate of third point
+
+            # Convert the points to binary vectors
+            vec1 = np.round([x1, y1, x2, y2, x3, y3])  # round to 0 or 1
+            vec2 = np.round([x2, y2, x3, y3, x1, y1])  # swap the order of points for vec2
+
+            # Calculate the Hamming distance between the binary vectors
+            distance = np.sum(vec1 != vec2)
+
+            return distance
+        else:
+            return 1
 
     def proj(r):
         return {"row": r, "x": cos(gap(r, A), gap(r, B), c)}
