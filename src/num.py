8ff60fa66882c9ec8cf6635682c38312bd292d96
Ritwik Tiwari <ritwiktiwari24@gmail.com>
2023-04-15 17:46:46 -0400

    Changes by urvashikar

--- src/num.py
+++ src/num.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 class Num:
-    def __init__(self, n=0, s="") -> None:
+    def __init__(self, n=0, s="", t=None) -> None:
         """
 
         Args:
@@ -18,3 +18,16 @@ class Num:
         self.hi = float("-inf")
         self.w = -1 if s.endswith("-") else 1
         self.isSym = False
+
+        self.mu = 0
+        self.m2 = 0
+        self.sd = 0
+        for x in t or []:
+            self.add(x)
+
+    def add(self, x):
+        self.n += 1
+        d = x - self.mu
+        self.mu = self.mu + d / self.n
+        self.m2 = self.m2 + d * (x - self.mu)
+        self.sd = 0 if self.n < 2 else (self.m2 / (self.n - 1)) ** 0.5
